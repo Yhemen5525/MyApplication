d4590698a4b2d766f818a215834caf40750ced15
Yhemen5525 <aagortey@gmail.com>
2019-05-27 03:09:09 +0000

    empty swapview book template

---
+++ app/src/main/java/com/whyread/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.whyread.myapplication;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.whyread.myapplication.fragments.PageFragment1;
+import com.whyread.myapplication.fragments.PageFragment2;
+import com.whyread.myapplication.fragments.PageFragment3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Fragment> list = new ArrayList<>();
+        list.add(new PageFragment1());
+        list.add(new PageFragment2());
+        list.add(new PageFragment3());
+
+        pager = findViewById(R.id.pager);
+        pagerAdapter = new SlidePagerAdapter(getSupportFragmentManager(),list);
+        pager.setAdapter(pagerAdapter);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/whyread/myapplication/fragments/PageFragment3.java
@@ -0,0 +1,25 @@
+package com.whyread.myapplication.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whyread.myapplication.R;
+
+public class PageFragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.page_3,
+                container,
+                false );
+
+        return rootView;
+    }
+}
